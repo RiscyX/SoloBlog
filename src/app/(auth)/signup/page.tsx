75485872aef4ba3dcfe14445b1afb66fc9b55ec9
Tóth Attila <tothattila5559@gmail.com>
2025-09-22 12:03:51 +0200

    Complete authentication flow using Supabase. Add signup, login and signout logic.

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,26 +1,15 @@
-import React from 'react'
+import React from "react";
+import styles from "../auth.module.css";
+import { signup } from "../actions";
 
-type Props = {}
+type Props = {};
 
 export default function SignUp({}: Props) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl font-bold mb-1">Sign Up</h1>
-      <form
-        className="flex flex-col items-center"
-        style={{
-          backgroundColor: "#1e1b3a",
-          width: "300px",
-          borderRadius: "8px",
-        }}
-      >
+      <form className={`flex flex-col items-center ${styles["auth-form"]}`}>
         <div>
-          <p className="mt-4 text-lg">Your Name</p>
-          <input
-            type="email"
-            name="email"
-            className="w-full p-2 border rounded"
-          />
         </div>
         <div>
           <p className="mt-4 text-lg">Email</p>
@@ -47,9 +36,8 @@ export default function SignUp({}: Props) {
           />
         </div>
         <button
-          type="submit"
-          className="w-full p-2 my-3 text-white rounded"
-          style={{ backgroundColor: "#cba6f7", cursor: "pointer", width: "209.6px" }}
+          formAction={signup}
+          className={`w-full p-2 my-3 text-white rounded ${styles["auth-button"]}`}
         >
           Login
         </button>
