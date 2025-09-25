d9b2b005e7d48b08868e174c611eb9e27425275d
Tóth Attila <tothattila5559@gmail.com>
2025-09-25 18:29:50 +0200

    Refactor: Auth forms and logic.

--- src/app/(auth)/confirm/route.ts
+++ src/app/(auth)/confirm/route.ts
@@ -1,5 +1,6 @@
 import { type EmailOtpType } from "@supabase/supabase-js";
 import { type NextRequest, NextResponse } from "next/server";
+
 import { createClient } from "@/utils/supabase/server";
 
 // Creating a handler to a GET request to route /auth/confirm
@@ -7,13 +8,7 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
-  const next = "/account";
-
-  // Create redirect link without the secret token
-  const redirectTo = request.nextUrl.clone();
-  redirectTo.pathname = next;
-  redirectTo.searchParams.delete("token_hash");
-  redirectTo.searchParams.delete("type");
+  const next = searchParams.get("next") ?? "/account";
 
   if (token_hash && type) {
     const supabase = await createClient();
@@ -23,12 +18,21 @@ export async function GET(request: NextRequest) {
       token_hash,
     });
     if (!error) {
-      redirectTo.searchParams.delete("next");
-      return NextResponse.redirect(redirectTo);
+      // Email confirmed successfully - redirect to main page
+      return NextResponse.redirect(new URL("/account", request.url));
+    } else {
+      // Error confirming email
+      return NextResponse.redirect(
+        new URL(
+          "/error?message=Invalid%20or%20expired%20confirmation%20link",
+          request.url
+        )
+      );
     }
   }
 
-  // return the user to an error page with some instructions
-  redirectTo.pathname = "/error";
-  return NextResponse.redirect(redirectTo);
+  // Invalid confirmation link
+  return NextResponse.redirect(
+    new URL("/error?message=Invalid%20confirmation%20link", request.url)
+  );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,47 +1,10 @@
-import React from "react";
-import styles from "../auth.module.css";
-import { signup } from "../actions";
+import SignUpForm from "./SignUpForm";
 
-type Props = {};
-
-export default function SignUp({}: Props) {
+export default function SignUpPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl font-bold mb-1">Sign Up</h1>
-      <form className={`flex flex-col items-center ${styles["auth-form"]}`}>
-        <div>
-        </div>
-        <div>
-          <p className="mt-4 text-lg">Email</p>
-          <input
-            type="email"
-            name="email"
-            className="w-full p-2 border rounded"
-          />
-        </div>
-        <div>
-          <p className="mt-4 text-lg">Password</p>
-          <input
-            type="password"
-            name="password"
-            className="w-full p-2 border rounded"
-          />
-        </div>
-        <div>
-          <p className="mt-4 text-lg">Password Again</p>
-          <input
-            type="password"
-            name="password"
-            className="w-full p-2 border rounded"
-          />
-        </div>
-        <button
-          formAction={signup}
-          className={`w-full p-2 my-3 text-white rounded ${styles["auth-button"]}`}
-        >
-          Login
-        </button>
-      </form>
+      <SignUpForm />
     </div>
   );
 }
